08fa941ded2be3e284376c44323a014d8e7505f9
Maija Y <maija.ylitalo@helsinki.fi>
2023-10-10 20:10:58 +0300

    matrix visualization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,110 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-export default function Home() {
+import { css } from "@emotion/css"
+import React, { useState } from 'react'
+
+const Page: React.FC<React.PropsWithChildren> = () => {
+  const [a, setA] = useState(1)
+  const [b, setB] = useState(0)
+  const [c, setC] = useState(0)
+  const [d, setD] = useState(1)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>        
+      <div className={css`
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+
+      `}>
+
+      <div className={css`
+        display: flex;
+        align-items: center;
+        padding: 1rem;
+
+      `}>
+        <div className={css`
+          height: 60px;
+          width: 4px;
+          padding-right: 5px;
+          border-left: 1px solid black;
+          border-top: 1px solid black;
+          border-bottom: 1px solid black;
+        `}/>
+
+        <div className={css`
+          display:inline-grid;
+          grid-template-columns: 50px 50px;
+          grid-column-gap: 8px;
+        `}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <input type="number" value={a} step=".1" onChange={(event) => setA(event.target.valueAsNumber)}/>
+            <input type="number" value={b} step=".1" onChange={(event) => setB(event.target.valueAsNumber)}/>
+            <input type="number" value={c} step=".1" onChange={(event) => setC(event.target.valueAsNumber)}/>
+            <input type="number" value={d} step=".1" onChange={(event) => setD(event.target.valueAsNumber)}/>
         </div>
+
+        <div className={css`
+          height: 60px;
+          width: 4px;
+          padding-left: 5px;
+          border-right: 1px solid black;
+          border-top: 1px solid black;
+          border-bottom: 1px solid black;
+        `}/>
+        </div>
+
+        <div className={css`
+          padding: 8px;
+        `}> &times; </div>
+
+        <div className={css`
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 150px;
+          height: 150px;
+        `}>
+          <img src="https://rage.github.io/introduction-to-matrix-calculus/static/c4b8116295cab8e3c208d824de6ffd01/5a46d/hedelmat.png"
+            width={120}
+            height={120}
+            alt="Picture of a fruitbowl" 
+          />
+        </div>
+        <div className={css`
+            display: flex;
+            align-items: center;
+          `}>
+          <p className={css`
+            padding: 8px;
+          `}> = </p>
+        
+          <div className={css` 
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            overflow: hidden;
+            width: 150px;
+            height: 150px;
+          `}>
+          <img src="https://rage.github.io/introduction-to-matrix-calculus/static/c4b8116295cab8e3c208d824de6ffd01/5a46d/hedelmat.png"
+            alt="Matrix linear transformation visualization"
+            width={120}
+            height={120} 
+            className={css`
+            transform: matrix(${a}, ${b}, ${c} , ${d}, 0, 0);          
+            `}
+          />
+        </div>
+        
+          
+        </div>
+        
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+    )
 }
+
+export default Page
